02cb4db7df4d0489bae2afb96013a4f559bfdaee
ranielzed <ranielzedcancino@gmail.com>
2025-01-20 19:10:56 +0800

    chore: bump version to 0.4.1 in Cargo.toml, world.wit, and update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #[allow(warnings)]
 mod bindings;
-use chrono::{NaiveDate, NaiveDateTime, Datelike};
 use serde_json::Value as JsonValue;
 
 use bindings::{
@@ -34,10 +33,6 @@ impl ExampleFdw {
     fn this_mut() -> &'static mut Self {
         unsafe { &mut (*INSTANCE) }
     }
-    fn parse_google_sheet_date(date_str: &str) -> Result<NaiveDate, FdwError> {
-        NaiveDate::parse_from_str(date_str, "%d-%b-%y")
-            .map_err(|e| format!("Failed to parse date: {}", e).into())
-    }
 }
 
 impl Guest for ExampleFdw {
@@ -48,112 +43,134 @@ impl Guest for ExampleFdw {
     }
 
     fn init(ctx: &Context) -> FdwResult {
-    Self::init_instance();
-    let this = Self::this_mut();
+        Self::init_instance();
+        let this = Self::this_mut();
 
-    // get API URL from foreign server options if it is specified
-    let opts = ctx.get_options(OptionsType::Server);
-    this.base_url = opts.require_or("base_url", "https://docs.google.com/spreadsheets/d");
+        // get API URL from foreign server options if it is specified
+        let opts = ctx.get_options(OptionsType::Server);
+        this.base_url = opts.require_or("base_url", "https://docs.google.com/spreadsheets/d");
 
-    Ok(())
-}
+        Ok(())
+    }
 
     fn begin_scan(ctx: &Context) -> FdwResult {
-    let this = Self::this_mut();
+        let this = Self::this_mut();
 
-    // get sheet id from foreign table options and make the request URL
-    let opts = ctx.get_options(OptionsType::Table);
-    let sheet_id = opts.require("sheet_id")?;
-    let url = format!("{}/{}/gviz/tq?tqx=out:json", this.base_url, sheet_id);
+        // get sheet id from foreign table options and make the request URL
+        let opts = ctx.get_options(OptionsType::Table);
+        let sheet_id = opts.require("sheet_id")?;
+        let url = format!("{}/{}/gviz/tq?tqx=out:json", this.base_url, sheet_id);
 
-    // make up request headers
-    let headers: Vec<(String, String)> = vec![
-        ("user-agent".to_owned(), "Sheets FDW".to_owned()),
-        // header to make JSON response more cleaner
-        ("x-datasource-auth".to_owned(), "true".to_owned()),
-    ];
+        // make up request headers
+        let headers: Vec<(String, String)> = vec![
+            ("user-agent".to_owned(), "Sheets FDW".to_owned()),
+            // header to make JSON response more cleaner
+            ("x-datasource-auth".to_owned(), "true".to_owned()),
+        ];
 
-    // make a request to Google API and parse response as JSON
-    let req = http::Request {
-        method: http::Method::Get,
-        url,
-        headers,
-        body: String::default(),
-    };
-    let resp = http::get(&req)?;
-    // remove invalid prefix from response to make a valid JSON string
-    let body = resp.body.strip_prefix(")]}'\n").ok_or("invalid response")?;
-    let resp_json: JsonValue = serde_json::from_str(body).map_err(|e| e.to_string())?;
+        // make a request to Google API and parse response as JSON
+        let req = http::Request {
+            method: http::Method::Get,
+            url,
+            headers,
+            body: String::default(),
+        };
+        let resp = http::get(&req)?;
+        // remove invalid prefix from response to make a valid JSON string
+        let body = resp.body.strip_prefix(")]}'\n").ok_or("invalid response")?;
+        let resp_json: JsonValue = serde_json::from_str(body).map_err(|e| e.to_string())?;
 
-    // extract source rows from response
-    this.src_rows = resp_json
-        .pointer("/table/rows")
-        .ok_or("cannot get rows from response")
-        .map(|v| v.as_array().unwrap().to_owned())?;
+        // extract source rows from response
+        this.src_rows = resp_json
+            .pointer("/table/rows")
+            .ok_or("cannot get rows from response")
+            .map(|v| v.as_array().unwrap().to_owned())?;
 
-    // output a Postgres INFO to user (visible in psql), also useful for debugging
-    utils::report_info(&format!(
-        "We got response array length: {}",
-        this.src_rows.len()
-    ));
+        // output a Postgres INFO to user (visible in psql), also useful for debugging
+        utils::report_info(&format!(
+            "We got response array length: {}",
+            this.src_rows.len()
+        ));
 
-    Ok(())
-}
+        Ok(())
+    }
 
     fn iter_scan(ctx: &Context, row: &Row) -> Result<Option<u32>, FdwError> {
-    let this = Self::this_mut();
+        let this = Self::this_mut();
 
-    // if all source rows are consumed, stop data scan
-    if this.src_idx >= this.src_rows.len() {
-        return Ok(None);
-    }
-
-    // extract current source row, an example of the source row in JSON:
-    // {
-    //   "c": [{
-    //      "v": 1.0,
-    //      "f": "1"
-    //    }, {
-    //      "v": "Erlich Bachman"
-    //    }, null, null, null, null, { "v": null }
-    //    ]
-    // }
-    let src_row = &this.src_rows[this.src_idx];
-
-    // loop through each target column, map source cell to target cell
-    for tgt_col in ctx.get_columns() {
-        let (tgt_col_num, tgt_col_name) = (tgt_col.num(), tgt_col.name());
-        if let Some(src) = src_row.pointer(&format!("/c/{}/v", tgt_col_num - 1)) {
-            // we only support I64 and String cell types here, add more type
-            // conversions if you need
-            let cell = match tgt_col.type_oid() {
-                TypeOid::I64 => src.as_f64().map(|v| Cell::I64(v as _)),
-                TypeOid::String => src.as_str().map(|v| Cell::String(v.to_owned())),
-                TypeOid::Date => src.as_str()
-                        .map(|v| Self::parse_google_sheet_date(v)
-                        .map(|dt| Cell::Date(dt)))
-                        .transpose()?,
-                _ => {
-                    return Err(format!(
-                        "column {} data type is not supported",
-                        tgt_col_name
-                    ));
-                }
-            };
-
-            // push the cell to target row
-            row.push(cell.as_ref());
-        } else {
-            row.push(None);
+        // if all source rows are consumed, stop data scan
+        if this.src_idx >= this.src_rows.len() {
+            return Ok(None);
         }
+
+        // extract current source row, an example of the source row in JSON:
+        // {
+        //   "c": [{
+        //      "v": 1.0,
+        //      "f": "1"
+        //    }, {
+        //      "v": "Erlich Bachman"
+        //    }, null, null, null, null, { "v": null }
+        //    ]
+        // }
+        let src_row = &this.src_rows[this.src_idx];
+
+        // loop through each target column, map source cell to target cell
+        for tgt_col in ctx.get_columns() {
+            let (tgt_col_num, tgt_col_name) = (tgt_col.num(), tgt_col.name());
+            if let Some(src) = src_row.pointer(&format!("/c/{}/v", tgt_col_num - 1)) {
+                // we only support I64 and String cell types here, add more type
+                // conversions if you need
+                let cell = match tgt_col.type_oid() {
+                    TypeOid::I64 => src.as_f64().map(|v| Cell::I64(v as _)),
+                    TypeOid::String => src.as_str().map(|v| Cell::String(v.to_owned())),
+                    TypeOid::Date => {
+                        if let Some(s) = src.as_str() {
+                            // Parse the string assuming RFC 3339 format, truncate time if necessary
+                            let ts = time::parse_from_rfc3339(s)?;
+                            let date_only = ts / 1_000_000; // Adjust if necessary to remove time part
+                            Some(Cell::Date(date_only))
+                        } else {
+                            None
+                        }
+                    }
+                    TypeOid::Timestamp => {
+                        if let Some(s) = src.as_str() {
+                            let ts = time::parse_from_rfc3339(s)?;
+                            Some(Cell::Timestamp(ts))
+                        } else {
+                            None
+                        }
+                    }
+                    TypeOid::Timestamptz => {
+                        if let Some(s) = src.as_str() {
+                            let ts = time::parse_from_rfc3339(s)?;
+                            Some(Cell::Timestamptz(ts))
+                        } else {
+                            None
+                        }
+                    }
+                    _ => {
+                        return Err(format!(
+                            "column {} data type is not supported",
+                            tgt_col_name
+                        ));
+                    }
+                };
+
+                // push the cell to target row
+                row.push(cell.as_ref());
+            } else {
+                row.push(None);
+            }
+        }
+
+        // advance to next source row
+        this.src_idx += 1;
+
+        // tell Postgres we've done one row, and need to scan the next row
+        Ok(Some(0))
     }
-
-    // advance to next source row
-    this.src_idx += 1;
-
-    // tell Postgres we've done one row, and need to scan the next row
-    Ok(Some(0))
-}
 
     fn re_scan(_ctx: &Context) -> FdwResult {
         Err("re_scan on foreign table is not supported".to_owned())
